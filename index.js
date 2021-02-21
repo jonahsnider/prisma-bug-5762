9cff7149cd8bb60d32d9fef7f8c7f2c2734397b1
Jonah Snider <jonah@jonah.pw>
2021-02-21 02:00:28 -0800

    feat: log Prisma events and throw error from main()

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const { PrismaClient } = require("@prisma/client");
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({ log: ["query", "info", "warn", "error"] });
 
 async function main() {
   const alpha = await prisma.alpha.create({ data: {} });
   await prisma.beta.create({ data: { alphaId: alpha.id } });
 }
 
-main();
+main().catch((error) => {
+  throw error;
+});
